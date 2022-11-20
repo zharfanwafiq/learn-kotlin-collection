83e93f0c96dda92a999c9286cb18b39bc370c25d
zharfanwafiq <zharfanww@gmail.com>
2022-11-20 09:23:41 +0700

    learn pair

---
+++ src/main/kotlin/pair/Pair.kt
@@ -0,0 +1,24 @@
+package pair
+
+fun main(){
+    pair()
+}
+
+fun pair() {
+    val pair = Pair(TAG,"Wafiq")
+
+    println(pair.first)
+    println(pair.second)
+
+    val pair2: Pair<String, Int> = Pair("String", 1)
+
+    println(pair2.first)
+    println(pair2.second)
+
+    val pair3: Pair<String, Int> = "String" to 1
+
+    println(pair3.first)
+    println(pair3.second)
+
+}
+const val TAG = "uwehaiofhapi"
